92d053faf5db20c68c908b6860743a2edc8552ca
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-11 15:53:24 -0500

    add #index method

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,2 +1,6 @@
-class LearningResourcesController < ApplicationController
+class Api::V1::LearningResourcesController < ApplicationController
+  def index
+    resources = LearningResourcesFacade.search_video(params[:country])
+    render json: LearningResourceSerializer.new(resources)
+  end
 end
