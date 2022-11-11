aecc148a3e1de85b5486cb4c9514464d7a1e6af5
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-11 11:10:50 -0500

    add set_country with if logic for blank country param

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::RecipesController < ApplicationController
+  before_action :set_country, only: %i(index)
+  
   def index
-    recipes = RecipesFacade.search_recipes(params[:country])
+    recipes = RecipesFacade.search_recipes(@country)
     render json: RecipeSerializer.new(recipes)
+  end
+
+  def set_country
+    if params[:country].blank?
+      @country = CountriesFacade.random_country
+    else
+      @country = params[:country]
+    end
   end
 end
