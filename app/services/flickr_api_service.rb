0d5c24a9f392340e0bda781960bc3550015b8a00
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-11 15:56:42 -0500

    add ::search flickr api

---
+++ app/services/flickr_api_service.rb
@@ -0,0 +1,21 @@
+class FlickrApiService
+  def self.conn
+    Faraday.new("https://api.flickr.com/services/rest/") do |f|
+      f.params['api_key'] = ENV['flickr_api_key']
+      f.headers['Content-Type'] = 'application/json'
+    end
+  end
+
+  def self.search(country)
+    response = conn.get do |f|
+      f.params['text'] = country
+      f.params['per_page'] = 10
+      f.params['method'] = 'flickr.photos.search'
+      f.params['format'] = 'json'
+      f.params['nojsoncallback'] = 1
+      f.params['content_type'] = 1
+      f.params['extras'] = 'url_c'
+    end
+    json(response)
+  end
+end
