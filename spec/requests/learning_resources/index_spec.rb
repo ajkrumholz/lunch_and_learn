bbf63e423eabff96e382ae0ed59afa390bbc4bdd
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-11 15:54:53 -0500

    update tests for expected json format

--- spec/requests/learning_resources/index_spec.rb
+++ spec/requests/learning_resources/index_spec.rb
@@ -1,11 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe "learning_resources#index" do
+RSpec.describe "learning_resources#index", :vcr do
   describe 'happy path' do
     describe 'when a request is made with a specific country' do
       it 'returns a json response' do
         country = 'laos'
         get "/api/v1/learning_resources?country=#{country}"
+        result = json(response)
+        expect(result).to have_key(:data)
+
+        data = result[:data]
+        expect(data).to be_a Hash
+        expect(data).not_to be_empty
+
+        expect(data).to have_key(:id)
+        expect(data[:id]).to eq("null")
+        expect(data).to have_key(:type)
+        expect(data[:type]).to eq("learning_resource")
+        expect(data).to have_key(:attributes)
+
+        attributes = data[:attributes]
+        expect(attributes).to have_key(:country)
+        expect(attributes).to have_key(:video)
+
+        video = attributes[:video]
+        expect(video).to have_key(:title)
+        expect(video).to have_key(:youtube_video_id)
+
+        expect(attributes).to have_key(:images)
+
+        images = attributes[:images]
+        images.each do |image|
+          expect(image).to have_key(:alt_tag)
+          expect(image).to have_key(:url)
+        end
       end
     end
   end
