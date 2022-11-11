29866ecd9b15c138246c2a383ef23424f4276340
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-11 15:53:59 -0500

    shorten faraday to f

--- app/services/edamam_api_service.rb
+++ app/services/edamam_api_service.rb
@@ -1,8 +1,8 @@
 class EdamamApiService
   def self.conn
-    Faraday.new(url: 'https://api.edamam.com') do |faraday|
-      faraday.params['app_id'] = ENV['edamam_app_id']
-      faraday.params['app_key'] = ENV['edamam_api_key']
+    Faraday.new(url: 'https://api.edamam.com') do |f|
+      f.params['app_id'] = ENV['edamam_app_id']
+      f.params['app_key'] = ENV['edamam_api_key']
     end
   end
   
