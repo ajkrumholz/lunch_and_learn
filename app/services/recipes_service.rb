8aab12e0b49568fd1e9ba3eef0d9c2bf65868e97
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-11 10:39:25 -0500

    add ::search_recipes(country)

---
+++ app/services/recipes_service.rb
@@ -0,0 +1,13 @@
+class RecipesService
+  def self.conn
+    Faraday.new(url: 'https://api.edamam.com') do |faraday|
+      faraday.params['app_id'] = ENV['edamam_app_id']
+      faraday.params['app_key'] = ENV['edamam_api_key']
+    end
+  end
+  
+  def self.search_recipes(country)
+    response = conn.get("/search?q=#{country}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
