e84e3572c2cebecf913cd3e56e5a6cf7bc176aab
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-11 11:12:35 -0500

    rename service

--- app/facades/recipes_facade.rb
+++ app/facades/recipes_facade.rb
@@ -1,6 +1,6 @@
 class RecipesFacade
   def self.search_recipes(country)
-    response = RecipesService.search_recipes(country)
+    response = EdamamApiService.search(country)
     recipe_array = response[:hits]
     recipe_array.map { |recipe_info| Recipe.new(recipe_info, country) }
   end
