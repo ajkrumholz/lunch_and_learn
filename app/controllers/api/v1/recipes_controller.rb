ca55d7cb4e3116162c50a0538589dbf992c8dc7d
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-11 11:26:48 -0500

    add logic to handle empty country param

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,12 +2,16 @@ class Api::V1::RecipesController < ApplicationController
   before_action :set_country, only: %i(index)
   
   def index
-    recipes = RecipesFacade.search_recipes(@country)
-    render json: RecipeSerializer.new(recipes)
+    if @country.blank?
+      render json: { data: [] }
+    else
+      recipes = RecipesFacade.search_recipes(@country)
+      render json: RecipeSerializer.new(recipes)
+    end
   end
 
   def set_country
-    if params[:country].blank?
+    if params[:country].nil?
       @country = CountriesFacade.random_country
     else
       @country = params[:country]
