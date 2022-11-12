a001e17cf0eaf70d876324720ae4baeff3074f83
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-12 11:03:38 -0500

    add and test favorites#create

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::FavoritesController < ApplicationController
+  def create
+    user = User.find_by_key(params[:favorite][:api_key])
+    if user.nil?
+      render json: CustomSerializer.bad_api_key, status: 401
+    else
+      fav = user.favorites.new(fav_params)
+      if fav.save
+        render json: CustomSerializer.favorite_success, status: 201
+      else
+        render json: CustomSerializer.errors(user.errors), status: 400
+      end
+    end
+  end
+
+  private
+
+  def fav_params
+    params.require(:favorite).permit(:country, :recipe_link, :recipe_title)
+  end
+end
---
+++ spec/requests/favorites/create_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe 'favorites#create', :vcr do
+  let!(:user) { User.create!(name: 'Gordon Ramsay', email: 'hotsteaks@steakmoney.com') }
+  let!(:country) { 'germany' }
+  let!(:recipe) { RecipesFacade.search_recipes(country).first }
+  let!(:headers) { {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  } }
+
+  describe 'when a post request is sent to /api/v1/favorites' do
+    describe 'happy path' do
+      it 'returns a json response' do
+        user.assign_api_key
+
+        body = { 
+          api_key: user.api_key,
+          country: country,
+          recipe_link: recipe.url,
+          recipe_title: recipe.title
+        }
+
+        post("/api/v1/favorites", params: { favorite: body }.to_json, headers: headers)
+
+        new_fav = Favorite.last
+        expect(new_fav.user).to eq(user)
+        expect(new_fav.country).to eq(country)
+        expect(new_fav.recipe_link).to eq(recipe.url)
+        expect(new_fav.recipe_title).to eq(recipe.title)
+
+        expect(response).to be_successful
+        expect(response).to have_http_status(201)
+
+        result = JSON.parse(response.body, symbolize_names: true)
+
+        expect(result).to have_key(:success)
+        expect(result[:success]).to eq("Favorite added successfully")
+      end
+    end
+
+    describe 'sad path' do
+      describe 'when an invalid api key is provided' do
+        it 'returns a 401 status with message' do
+          user.assign_api_key
+
+            body = { 
+              api_key: "bad key",
+              country: country,
+              recipe_link: recipe.url,
+              recipe_title: recipe.title
+            }
+
+            post("/api/v1/favorites", params: { favorite: body }.to_json, headers: headers)
+
+            expect(response).not_to be_successful
+            expect(response).to have_http_status(401)
+
+            result = JSON.parse(response.body, symbolize_names: true)
+
+            expect(result).to have_key(:errors)
+            expect(result[:errors]).to include("API Key could not be verified")
+        end
+      end
+    end
+  end
+end
