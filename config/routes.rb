97a9f11b48bed6422f605797e74891f71f300e3d
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-12 10:33:11 -0500

    add resources for favorites

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :recipes, only: %i(index)
       resources :learning_resources, only: %i(index)
       resources :users, only: %i(create)
+      resources :favorites, only: %i(create)
     end
   end
 end
