d16574fad2db96c3dd222e9315676b841c682db9
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-12 11:03:21 -0500

    allow error serializer to handle multiple objects

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
       user.assign_api_key
       render json: UserSerializer.new(user), status: 201
     else
-      render json: CustomSerializer.user_errors(user.errors), status: 400
+      render json: CustomSerializer.errors(user.errors), status: 400
     end
   end
 
