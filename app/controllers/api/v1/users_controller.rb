adca2c02eebfad2c47b377b3caff914683e559bc
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-12 09:07:44 -0500

    add user#create with strong params

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      user.assign_api_key
+      render json: UserSerializer.new(user), status: 201
+    else
+      render json: CustomSerializer.user_errors(user.errors), status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
