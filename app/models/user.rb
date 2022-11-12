11d8f5cdb35ccbffc588f6c63d6aaa2123298f4f
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-12 09:02:45 -0500

    test instance methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   validates_presence_of :name, :email
+
+  def assign_api_key
+    update(api_key: generate_api_key)
+  end
+
+  def generate_api_key
+    characters = (0..9).to_a + ('a'..'z').to_a
+    characters.sample(20).join
+  end
 end
