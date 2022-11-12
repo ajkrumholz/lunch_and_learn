dcf8748000b4606771913e48e770f8b51adb96e7
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-12 09:45:03 -0500

    move caching to service

--- app/facades/countries_facade.rb
+++ app/facades/countries_facade.rb
@@ -1,13 +1,7 @@
 class CountriesFacade
   def self.random_country
-    response = fetch_country_list
+    response = RestCountriesService.all
     name_array = response.map { |country| country[:name] }
     name_array.sample
-  end
-  
-  def self.fetch_country_list
-    Rails.cache.fetch('country list', expires_in: 30.days) do
-      RestCountriesService.all
-    end
   end
 end
--- app/services/rest_countries_service.rb
+++ app/services/rest_countries_service.rb
@@ -4,7 +4,9 @@ class RestCountriesService
   end
 
   def self.all
-    response = conn.get("/v2/all?fields=name")
-    JSON.parse(response.body, symbolize_names: true)
+    Rails.cache.fetch("countries list", expires_in: 30.days) do
+      response = conn.get("/v2/all?fields=name")
+      JSON.parse(response.body, symbolize_names: true)
+    end
   end
 end
