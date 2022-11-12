881511b1ceb2e88bc8dce3d435ae978ad37e4eee
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-12 12:04:21 -0500

    add happy path test for empty favorites

--- spec/requests/favorites/index_spec.rb
+++ spec/requests/favorites/index_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'favorites#index' do
   let!(:user) { create :user }
   let!(:favorites) { create_list(:favorite, 5, user: user ) }
+  let!(:other_user) { create :user }
   let!(:headers) { {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
@@ -15,10 +16,10 @@ RSpec.describe 'favorites#index' do
           user.assign_api_key
 
           body = {
-            api_key: user.api_key
+            'api_key': user.api_key
           }
 
-          get '/api/v1/favorites', params: body.to_json, headers: headers
+          get('/api/v1/favorites', params: { favorite: body }, headers: headers)
 
           expect(response).to be_successful
           expect(response).to have_http_status(200)
@@ -28,12 +29,15 @@ RSpec.describe 'favorites#index' do
           expect(result).to have_key(:data)
 
           data = result[:data]
-          expect(data).to have_key(:id)
-          expect(data).to have_key(:type)
-          expect(data[:type]).to eq('favorite')
-          expect(data).to have_key(:attributes)
+          expect(data).to be_an Array
 
-          attributes = data[:attributes]
+          favorite = data.first
+          expect(favorite).to have_key(:id)
+          expect(favorite).to have_key(:type)
+          expect(favorite[:type]).to eq('favorite')
+          expect(favorite).to have_key(:attributes)
+
+          attributes = favorite[:attributes]
           expect(attributes).to have_key(:recipe_title)
           expect(attributes[:recipe_title]).to be_a String
           expect(attributes).to have_key(:recipe_link)
@@ -42,6 +46,24 @@ RSpec.describe 'favorites#index' do
           expect(attributes[:country]).to be_a String
           expect(attributes).to have_key(:created_at)
           expect(attributes[:created_at]).to be_a String
+        end
+
+        it 'returns an empty data response if user has no favorites' do
+          other_user.assign_api_key
+
+          body = {
+            'api_key': other_user.api_key
+          }
+
+          get('/api/v1/favorites', params: { favorite: body }, headers: headers)
+
+          expect(response).to be_successful
+          expect(response).to have_http_status(200)
+          
+          result = JSON.parse(response.body, symbolize_names: true)
+
+          expect(result).to have_key(:data)
+          expect(result[:data]).to be_empty
         end
       end
     end
