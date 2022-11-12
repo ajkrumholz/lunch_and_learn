1815b4f20fd8070aa400d6cb9d255288a0b29a74
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-12 09:36:18 -0500

    add user error methods

--- app/serializers/custom_serializer.rb
+++ app/serializers/custom_serializer.rb
@@ -2,4 +2,19 @@ class CustomSerializer
   def self.no_recipe_content
     { data: [] }
   end
+
+  def self.user_errors(errors)
+    { errors: error_array(errors.messages) }
+  end
+
+  def self.error_array(messages)
+    messages.reduce([]) do |array, (att,msg)|
+      title = att.to_s.humanize
+      msgs = msg.map { |msg| title + " " + msg }
+      array << { 
+        source: att,
+        detail: msgs
+      }
+    end
+  end
 end
