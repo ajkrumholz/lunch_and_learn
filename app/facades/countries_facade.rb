19696e4098b68f918d49044701dccc3c50e70aac
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-12 09:43:00 -0500

    add caching to countries facade

--- app/facades/countries_facade.rb
+++ app/facades/countries_facade.rb
@@ -1,7 +1,13 @@
 class CountriesFacade
   def self.random_country
-    response = RestCountriesService.all
+    response = fetch_country_list
     name_array = response.map { |country| country[:name] }
     name_array.sample
+  end
+  
+  def self.fetch_country_list
+    Rails.cache.fetch('country list', expires_in: 30.days) do
+      RestCountriesService.all
+    end
   end
 end
