d97d04c8b550a149c74cf5946ef5146894303c89
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-12 12:19:39 -0500

    cache photos search for 1 week

--- app/services/flickr_api_service.rb
+++ app/services/flickr_api_service.rb
@@ -7,15 +7,21 @@ class FlickrApiService
   end
 
   def self.search(country)
-    response = conn.get do |f|
-      f.params['text'] = country
-      f.params['per_page'] = 10
-      f.params['method'] = 'flickr.photos.search'
-      f.params['format'] = 'json'
-      f.params['nojsoncallback'] = 1
-      f.params['content_type'] = 1
-      f.params['extras'] = 'url_c'
-    end
+    response = search_uncached(country)
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.search_uncached(country)
+    Rails.cache.fetch("photos_#{country}", expires_in: 7.days) do
+      conn.get do |f|
+        f.params['text'] = country
+        f.params['per_page'] = 10
+        f.params['method'] = 'flickr.photos.search'
+        f.params['format'] = 'json'
+        f.params['nojsoncallback'] = 1
+        f.params['content_type'] = 1
+        f.params['extras'] = 'url_c'
+      end
+    end
   end
 end
