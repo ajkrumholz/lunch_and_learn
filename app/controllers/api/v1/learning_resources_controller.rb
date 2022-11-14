80077fb1de671cc4592eddc6f41e09e764503dfa
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-14 13:23:04 -0500

    add random country in case of no country param

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::LearningResourcesController < ApplicationController
+  before_action :set_country, only: :index
+  
   def index
-    resources = LearningResourcesFacade.search_video(params[:country])
+    resources = LearningResourcesFacade.search_video(@country)
     render json: LearningResourceSerializer.new(resources)
+  end
+
+  private 
+
+  def set_country
+    if params[:country].nil?
+      @country = CountriesFacade.random_country
+    else
+      @country = params[:country]
+    end
   end
 end
--- spec/requests/learning_resources/index_spec.rb
+++ spec/requests/learning_resources/index_spec.rb
@@ -31,10 +31,56 @@ RSpec.describe "learning_resources#index", :vcr do
 
         images = attributes[:images]
         images.each do |image|
-          expect(image).to have_key(:alt_tag)
-          expect(image).to have_key(:url)
+        expect(image).to have_key(:alt_tag)
+        expect(image).to have_key(:url)
         end
       end
+    end
+
+    describe 'when country param is omitted' do
+      it 'searches a random country instead' do
+        allow(CountriesFacade).to receive(:random_country).and_return("germany")
+
+        get "/api/v1/learning_resources"
+        result = json(response)
+        expect(result).to have_key(:data)
+
+        data = result[:data]
+        expect(data).to be_a Hash
+        expect(data).not_to be_empty
+
+        expect(data).to have_key(:id)
+        expect(data[:id]).to eq("null")
+        expect(data).to have_key(:type)
+        expect(data[:type]).to eq("learning_resource")
+        expect(data).to have_key(:attributes)
+
+        attributes = data[:attributes]
+        expect(attributes).to have_key(:country)
+        expect(attributes).to have_key(:video)
+
+        video = attributes[:video]
+        expect(video).to have_key(:title)
+        expect(video).to have_key(:youtube_video_id)
+
+        expect(attributes).to have_key(:images)
+
+        images = attributes[:images]
+        images.each do |image|
+        expect(image).to have_key(:alt_tag)
+        expect(image).to have_key(:url)
+        end
+      end
+    end
+  end
+
+  describe 'sad path' do
+    describe 'when country param is blank' do
+
+    end
+
+    describe 'when country param returns no results' do
+
     end
   end
 end
