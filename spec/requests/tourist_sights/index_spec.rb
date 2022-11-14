ea3a6146605237c64a623bdb89d83b49c9ec4465
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-14 11:53:35 -0500

    rearrange happy path nesting expectations

--- spec/requests/tourist_sights/index_spec.rb
+++ spec/requests/tourist_sights/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'tourist_sights#index' do
+RSpec.describe 'tourist_sights#index', :vcr do
   describe 'happy path' do
     describe 'when a GET request is made to /api/v1/tourist_sights' do
       it 'returns a list of tourist sights within 20k meters of a country capitol city' do
@@ -14,6 +14,20 @@ RSpec.describe 'tourist_sights#index' do
         result = JSON.parse(response.body, symbolize_names: true)
         expect(result).to have_key(:data)
         expect(result[:data]).not_to be_empty
+        expect(result[:data]).to be_an Array
+
+        first_result = result[:data].first
+        expect(first_result).to have_key(:id)
+        expect(first_result[:id]).to eq('null')
+        expect(first_result).to have_key(:type)
+        expect(first_result[:type]).to eq('tourist_sight')
+        expect(first_result).to have_key(:attributes)
+
+        attributes = first_result[:attributes]
+        expect(attributes).to be_a Hash
+        expect(attributes).to have_key(:name)
+        expect(attributes).to have_key(:address)
+        expect(attributes).to have_key(:place_id)
       end
     end
   end
