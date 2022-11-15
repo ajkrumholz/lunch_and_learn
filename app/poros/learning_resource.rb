9cd4292133c397b0e5b2999339d1746b14d63df3
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-15 10:59:38 -0500

    remove comments

--- app/poros/learning_resource.rb
+++ app/poros/learning_resource.rb
@@ -10,22 +10,4 @@ class LearningResource
     @video = video
     @images = images
   end
-
-  # def video
-  #   return [] if @video_data[:items].empty?
-  #   { 
-  #     title: @video_data[:items].first[:snippet][:title],
-  #     youtube_video_id: @video_data[:items].first[:id][:videoId]   
-  #   }
-  # end
-
-  # def images
-  #   return [] if @image_data[:photos][:photo].empty?
-  #   @image_data[:photos][:photo].inject([]) do |array, photo|
-  #     array << { 
-  #       alt_tag: photo[:title],
-  #       url: photo[:url_c]
-  #     }
-  #   end
-  # end
 end
