860148d98ec573ad7e413ed4169dbb723d8836a1
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-15 11:09:52 -0500

    test convert methods

--- spec/facades/learning_resources_facade_spec.rb
+++ spec/facades/learning_resources_facade_spec.rb
@@ -13,4 +13,43 @@ RSpec.describe LearningResourcesFacade, :vcr do
       expect(result.video).to be_a Hash
     end
   end
+
+  describe '::convert_v(video_data)' do
+    it 'converts service response into hash for poro' do
+      country = 'indonesia'
+      hash = YoutubeApiService.search(country)
+
+      expect(hash).to have_key(:items)
+
+      result = described_class.convert_v(hash)
+      expect(result).to be_a Hash
+      expect(result).not_to have_key(:items)
+      expect(result).to have_key(:title)
+      expect(result).to have_key(:youtube_video_id)
+    end
+  end
+
+  describe '::convert_i(image_data)' do
+    it 'converts service response into array for poro' do
+      country = 'indonesia'
+      hash = FlickrApiService.search(country)
+
+      expect(hash).to be_a Hash
+      expect(hash).to have_key(:photos)
+      expect(hash[:photos]).to have_key(:page)
+      expect(hash[:photos]).to have_key(:photo)
+      photo = hash[:photos][:photo].first
+
+      expect(photo).to have_key(:id)
+      expect(photo).to have_key(:owner)
+
+      result = described_class.convert_i(hash)
+      expect(result).to be_an Array
+      expect(result.first).to be_a Hash
+      expect(result.first).to have_key(:alt_tag)
+      expect(result.first).to have_key(:url)
+      expect(result.first).not_to have_key(:id)
+      expect(result.first).not_to have_key(:owner)
+    end
+  end
 end
