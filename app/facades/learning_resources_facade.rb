ac9137ca2ab7e95e86c5ef810dd12735e76b81c1
A.J. Krumholz <krumholz.alan@gmail.com>
2022-11-15 10:59:21 -0500

    move poro logic to facade

--- app/facades/learning_resources_facade.rb
+++ app/facades/learning_resources_facade.rb
@@ -1,7 +1,29 @@
 class LearningResourcesFacade
   def self.search(country)
-    video_data = YoutubeApiService.search(country)
-    image_data = FlickrApiService.search(country)
+    video_data = convert_v(YoutubeApiService.search(country))
+    image_data = convert_i(FlickrApiService.search(country))
     LearningResource.new(video_data, image_data, country)
+  end
+
+  private
+
+  def self.convert_v(video_data)
+    items = video_data[:items]
+    return [] if items.empty?
+    { 
+      title: items.first[:snippet][:title],
+      youtube_video_id: items.first[:id][:videoId]   
+    }
+  end
+
+  def self.convert_i(image_data)
+    photos = image_data[:photos][:photo]
+    return [] if photos.empty?
+    photos.inject([]) do |photo_array, photo|
+      photo_array << { 
+        alt_tag: photo[:title],
+        url: photo[:url_c]
+      }
+    end
   end
 end
--- app/poros/learning_resource.rb
+++ app/poros/learning_resource.rb
@@ -1,29 +1,31 @@
 class LearningResource
   attr_reader :id,
-              :country
+              :country,
+              :video,
+              :images
 
-  def initialize(video_data, image_data, country)
+  def initialize(video, images, country)
     @id = "null"
     @country = country
-    @video_data = video_data
-    @image_data = image_data
+    @video = video
+    @images = images
   end
 
-  def video
-    return [] if @video_data[:items].empty?
-    { 
-      title: @video_data[:items].first[:snippet][:title],
-      youtube_video_id: @video_data[:items].first[:id][:videoId]   
-    }
-  end
+  # def video
+  #   return [] if @video_data[:items].empty?
+  #   { 
+  #     title: @video_data[:items].first[:snippet][:title],
+  #     youtube_video_id: @video_data[:items].first[:id][:videoId]   
+  #   }
+  # end
 
-  def images
-    return [] if @image_data[:photos][:photo].empty?
-    @image_data[:photos][:photo].inject([]) do |array, photo|
-      array << { 
-        alt_tag: photo[:title],
-        url: photo[:url_c]
-      }
-    end
-  end
+  # def images
+  #   return [] if @image_data[:photos][:photo].empty?
+  #   @image_data[:photos][:photo].inject([]) do |array, photo|
+  #     array << { 
+  #       alt_tag: photo[:title],
+  #       url: photo[:url_c]
+  #     }
+  #   end
+  # end
 end
